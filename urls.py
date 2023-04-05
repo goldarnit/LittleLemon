8c49d8605e42655c4da5910cd36129ec95233057
goldarnit <goldarnits@gmail.com>
2023-04-05 15:53:30 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
-"""LittleLemon URL Configuration
+"""
+URL configuration for littlelemon project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),        
-    path('api/', include('LittleLemonAPI.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
-
 ]
